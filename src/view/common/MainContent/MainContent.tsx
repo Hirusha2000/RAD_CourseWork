8dc7e1e070cb7052d2b113bd821f45a833d8d9d7
Hirusha2000 <2000hirushaudara@gmail.com>
2023-12-29 21:49:58 +0530

    new updatws

--- src/view/common/MainContent/MainContent.tsx
+++ src/view/common/MainContent/MainContent.tsx
@@ -1,21 +1,20 @@
 import {Component} from "react";
-import {Route, Routes} from "react-router-dom";
-import {Home} from "../../pages/Home/Home";
-import {About} from "../../pages/About/About";
-import {Contact} from "../../pages/Contact/Contact";
 
 export class MainContent extends Component {
     render() {
         return (
-            <div className="pt-28 pb-28 mx-auto bg-amber-100">
-                {/*<h1 className="text-4xl text-center text-tertiary">*/}
-                {/*    This is Main Content!</h1>*/}
+            <div className="pt-28 pb-28 bg-amber-100">
+                <h1 className="text-4xl text-center text-tertiary">
+                    This is Main Content!</h1>
 
-                <Routes>
-                    <Route path="/" Component={Home}></Route>
-                    <Route path="/about" Component={About}></Route>
-                    <Route path="/contact" Component={Contact}></Route>
-                </Routes>
+                <div className="h-20 w-20 text-center p-1 bg-yellow-400
+                                rotate-12 shadow-gray-900 shadow-xl
+                                hover:shadow-red-500
+                                !cursor-pointer
+                                min-h-screen max-h-screen
+                                mx-auto">
+                    Height <br/>& <br/> Width
+                </div>
             </div>
         );
     }
