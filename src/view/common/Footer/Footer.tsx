6d2e173fe818fcf1ad5904ca08fa76edf0ca1418
Hirusha2000 <2000hirushaudara@gmail.com>
2024-01-01 22:47:51 +0530

    footer updates

--- src/view/common/Footer/Footer.tsx
+++ src/view/common/Footer/Footer.tsx
@@ -1,26 +1,28 @@
 import {Component} from "react";
-import logo from '../../../images/icon.png'
+import logo from '../../../images/darshana.svg'
 import {Link} from "react-router-dom";
 
 export class Footer extends Component {
     render() {
+
+
         return (
-            <div className="p-2 bg-[#444544] flex
-                            justify-center">
+            <div className="p-2 bg-[#808080] flex justify-center">
 
-                <p className="text-[11px] text-[#e6f0e6]
-                              pr-2 pt-1 hover:text-green-400">
-                    Copyright @ 2023</p>
-
-                <Link to="/">
                     <div className="flex">
                         <h1 className="text-1xl text-secondary">
-                            Organic Shop</h1>
-                        <img className="h-5 w-5 top-0 ml-1"
-                             src={logo} alt=""/>
+          <span style={{ fontFamily: '', fontSize: '12' }}>
+            All Rights Reserved by&nbsp;
+          </span>    <Link to="/">
+                            <span style={{ color: 'whitesmoke' }}>Darshana Mobile (PVT) LTD</span>
+
+                        </Link>  @2024.
+                        </h1>
+
                     </div>
-                </Link>
+
             </div>
         );
+
     }
 }
